721b413b71811b1bc9274324ad4ebfa3121c4f9f
Vanessa <hello@vfernandes.dev>
2022-04-07 19:40:15 +0200

    Edit auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
-const Ranger = require("../models/Ranger.model");
 const bcrypt = require("bcrypt");
 
 // Sign up for User 
 router.post("/signup/user", async (req, res, next) => {
     try{
         const {username, email, password } = req.body
+        console.log(req.body);
         const userExists = await User.findOne({username});
 
         if(userExists) {
@@ -21,46 +21,20 @@ router.post("/signup/user", async (req, res, next) => {
             email,
             password: passwordHash
         } 
+        req.session.currentUser = user;
+        console.log("User session exists?", req.session.currentUser); 
         await User.create(user);
 
         return res.json({message: "Successfully signed up user"})
 
     } catch(err) {
-        console.err("Error if user exists from auth.routes", err);
-        return res.status(400).json({errorMessage: "Uppss here went something wrong."});
-    }
-})
-
-// Sign up for Ranger 
-router.post("/signup/ranger", async (req, res, next) => {
-    try{
-        const {rangername, email, password } = req.body
-        const rangerExists = await Ranger.findOne({rangername});
-
-        if(rangerExists) {
-            throw Error ("This User/Ranger already exists. Please choose another name.");
-        };
-
-        const salt = await bcrypt.genSalt(10);
-        const passwordHash = await bcrypt.hash(password, salt);
-
-        const ranger = {
-            rangername,
-            email,
-            password: passwordHash
-        } 
-        await Ranger.create(user);
-
-        return res.json({message: "Successfully signed up ranger"})
-
-    } catch(err) {
-        console.err("Error if ranger exists from auth.routes", err);
-        return res.status(400).json({errorMessage: "Uppss here went something wrong."});
+        console.error("Error if user exists from auth.routes", err);
+        return res.status(400).json({errorMessage: "Uppss here went something wrong." + err});
     }
 })
 
 // Login for User
-router.post("/login/user", async (res, req, next) => {
+router.post("/login/user", async (req, res, next) => {
     try{
         console.log("line 65 from auth.routes on server", req.body);
         const { email, password } = req.body;
@@ -76,40 +50,71 @@ router.post("/login/user", async (res, req, next) => {
         }
         req.session.currentUser = user;
         console.log("User session exist?", req.session.currentUser); 
-        return res.json({ message: "Successfully logged in!", user: sessionUser });
+        return res.json({ message: "Successfully logged in!", user: req.session.currentUser });
 
     } catch(err) {
         console.error("Error from line 82 in auth.routes", err);
-        return res.status(400).json({errorMessage: "Email or password don't match"});
+        return res.status(400).json({errorMessage: "Email or password don't match" + err});
+
+        // return res.status(400).json({errorMessage: "Email or password don't match"});
     }
 
 })
 
-//Login for Ranger
-router.post("/login/ranger", async (res, req, next) => {
-    try{
-        console.log("line 90 from auth.routes on server", req.body);
-        const { email, password } = req.body;
-        const ranger = await Ranger.findOne({email});
+// // Sign up for Ranger 
+// router.post("/signup/ranger", async (req, res, next) => {
+//     try{
+//         const {rangername, email, password } = req.body
+//         const rangerExists = await Ranger.findOne({rangername});
 
-        if (!ranger) {
-            throw Error();
-        };
-        const checkedPassword = await bcrypt.compare(password, ranger.password);
+//         if(rangerExists) {
+//             throw Error ("This User/Ranger already exists. Please choose another name.");
+//         };
 
-        if(!checkedPassword) {
-            throw Error()
-        }
-        req.session.currentRanger = ranger;
-        console.log("Ranger session exists?", req.session.currentRanger); 
-        return res.json({ message: "Successfully logged in!", user: sessionUser });
+//         const salt = await bcrypt.genSalt(10);
+//         const passwordHash = await bcrypt.hash(password, salt);
 
-    } catch(err) {
-        console.error("Error from line 82 in index.routes", err);
-        return res.status(400).json({errorMessage: "Email or password don't match"});
-    }
+//         const ranger = {
+//             rangername,
+//             email,
+//             password: passwordHash
+//         } 
+//         await Ranger.create(user);
 
-})
+//         return res.json({message: "Successfully signed up ranger"})
+
+//     } catch(err) {
+//         console.err("Error if ranger exists from auth.routes", err);
+//         return res.status(400).json({errorMessage: "Uppss here went something wrong."});
+//     }
+// })
 
 
-module.exports = router
+// //Login for Ranger
+// router.post("/login/ranger", async (res, req, next) => {
+//     try{
+//         console.log("line 90 from auth.routes on server", req.body);
+//         const { email, password } = req.body;
+//         const ranger = await Ranger.findOne({email});
+
+//         if (!ranger) {
+//             throw Error();
+//         };
+//         const checkedPassword = await bcrypt.compare(password, ranger.password);
+
+//         if(!checkedPassword) {
+//             throw Error()
+//         }
+//         req.session.currentRanger = ranger;
+//         console.log("Ranger session exists?", req.session.currentRanger); 
+//         return res.json({ message: "Successfully logged in!", user: sessionUser });
+
+//     } catch(err) {
+//         console.error("Error from line 82 in index.routes", err);
+//         return res.status(400).json({errorMessage: "Email or password don't match"});
+//     }
+
+// })
+
+
+module.exports = router;
