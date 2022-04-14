a8b8e5c7b515bd6e49120cbf3529c695cd0e7424
Vanessa <hello@vfernandes.dev>
2022-04-14 20:57:36 +0200

    add csrf middleware

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
+const csrfMiddleware = require("../middlewares/csrfMiddleware");
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
+});
+
+router.get("/getCsrfToken", csrfMiddleware, (req, res, next) => {
+  res.json({ csrfToken: req.csrfToken() });
 });
 
 // You put the next routes here 👇
